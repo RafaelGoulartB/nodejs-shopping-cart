953ec7996628be581ed41c8d5958e5ca2606c0b9
RafaelGoulartB <rafaelgoulartb@gmail.com>
2019-03-07 16:27:23 -0300

    Index products with DB

---
+++ dao/productsDAO.js
@@ -0,0 +1,22 @@
+class productsDAO {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  list(limmit) {
+    return new Promise((resolve, reject) => {
+      if(!limmit) {
+        this.connection.query('select * from products', (error, result) => {
+          if(error) return reject(error);
+          return resolve(result);
+        })
+      }
+      this.connection.query('select * from products limit ?', limmit, (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    }); // End Promise
+  }; // End list
+}
+
+module.exports = () => productsDAO;
--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,17 @@
 module.exports = app => {
 
   app.get('/', (req, res) => {
-    res.render('shop/index', {title: 'hbs'});
+    const connection = app.dao.connectionFactory();
+    const productsDAO = new app.dao.productsDAO(connection);
+    productsDAO.list()
+      .then(products => {
+        console.log(products);
+        res.status(200).render('shop/index', {title: 'Shopping Cart', products: products})
+      }
+      )
+      .catch(err => res.status(400).send(err));
+
+
   });
 
 }
